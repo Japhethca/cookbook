cc7a1938a2f09cd5511a5c2e425fe562a012d7f9
Anyigor Chidiebere <chidiebere.anyigor@gmail.com>
2020-06-12 14:27:35 +0100

    handle non existing routes (#9)

--- src/rest/apiRoutes.ts
+++ src/rest/apiRoutes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import authRoutes from './authenticaton/routes';
 import recipesRoutes from './recipes/routes';
