788bd2fc5ddfcb8613bec09bbf12abab6f730c97
japhethca <chidiebere.anyigor@gmail.com>
2020-06-20 13:50:48 +0100

    convert project to mono client server repo
    - create web package
    - add api package

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
-import restRoutes from './rest/apiRoutes';
+import restRoutes from './rest/routes';
 
 const app: Application = express();
 
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,12 @@
 import { createConnection } from 'typeorm';
 
+import dbconf from './confs/dbConf';
 import { runserver, RestServer } from './server';
 import app from './app';
 
 (async () => {
   try {
-    await createConnection();
+    await createConnection(dbconf);
   } catch (dberr) {
     console.log(dberr, 'error from database');
   }
